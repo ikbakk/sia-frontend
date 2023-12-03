4fc651a405c101ad00dad6e39f2f2b8e659fd759
ikbakk <iqbalfirdaus05@gmail.com>
2023-12-04 01:25:46 +0800

    add logout button

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,9 +1,19 @@
+import { cookies } from 'next/headers';
 import HeaderAvatar from '../molecules/Header/HeaderAvatar';
 import ThemeSwitcher from '../molecules/Header/ThemeSwitcher';
+import { Button } from '../ui/button';
+import { redirect } from 'next/navigation';
 
 type HeaderProps = { name: string; semester: number };
 
 const Header = ({ name, semester }: HeaderProps) => {
+  const handleLogout = async () => {
+    'use server';
+
+    const cookieStore = cookies();
+    cookieStore.delete('token');
+    redirect('/');
+  };
   return (
     <header className='flex w-full items-center justify-between bg-background px-4 py-2'>
       <ThemeSwitcher />
@@ -13,6 +23,12 @@ const Header = ({ name, semester }: HeaderProps) => {
           <h4 className='text-sm text-primary/50'>Semester {semester}</h4>
         </section>
         <HeaderAvatar fallbackLabel='MF' />
+        <form>
+          <input type='hidden' name='logout' />
+          <Button type='submit' formAction={handleLogout}>
+            Logout
+          </Button>
+        </form>
       </div>
     </header>
   );
